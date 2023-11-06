c2b4faafeed7ad64a42e86c58bde86da82e45389
ByFindIt152 <jorgearocutipa036@gmail.com>
2023-11-06 11:29:59 -0500

    Cambios

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Actividad from "./componentes/Actividad";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Actividad />
+    </div>
+
+  );
+}
+
+export default App;
