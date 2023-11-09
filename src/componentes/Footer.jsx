ffaa11869eda02864d1c84686dc15a974aab8a58
AdrianParedesG <adrian.paredes3210@gmail.com>
2023-11-09 16:25:37 -0500

    arreglo de cards en footer

--- src/componentes/Footer.jsx
+++ src/componentes/Footer.jsx
@@ -11,7 +11,7 @@ function Footer() {
             <div className="container-ayuda">
               <h1 className="titulo-1">¿ Necesitas ayuda? </h1>
               <h1 className="titulo-2">¡Ponte en <span className="text-color">contacto con nosotros!</span></h1>
-              <div className="cards">
+              <div className="cards-footer">
                 <div className="card-izquierda">
                   <h1 className="card-titulo-1">Soporte de Ayuda</h1>
                   <h1 className="card-titulo-2">¿Tienes problemas para usar nuestra app?</h1>
