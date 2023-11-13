de3fa8101eb7c3c0182b4c808256f23a1f9b87b0
Srcox100 <lcox.esc@gmail.com>
2023-11-13 15:07:56 -0500

    Avances Responsive Tablet Vertical

--- src/componentes/Usuario.jsx
+++ src/componentes/Usuario.jsx
@@ -36,6 +36,10 @@ function Usuario() {
             <div>
                 <img className="img1" src={Img1} alt="Img1" />
             </div>
+            <div className="contenedor-002">
+                <button className="boton-1">Registrarme</button>
+                <img className="google" src={Google} alt="Google" />
+            </div>
             <div>
                 <h1 className="texto-01">
                     <span>Encuentra</span> los mejores productos  para tu negocio en <span>Kampanee</span>
@@ -59,29 +63,47 @@ function Usuario() {
                 </div>
             </div>
 
-            
+
             <div>
                 <img className="img2" src={Img2} alt="Img2" />
             </div>
-            <div className="contenedor04">
-                <img className="img3" src={Img3} alt="Img3" />
-                <div className="logo-container">
+
+            <div className="general">
+                <div className="contenedor04">
                     <img className="logo0" src={Logo} alt="Logo" />
-                </div>
-                <div>
-                    <h1 className="texto-001">
+                    <img className="logo0-res" src={Logo} alt="Logo" />
+                    <h1 className="texto-001-res">
                         <span>Kampanee</span> te acerca a tus proveedores favoritos
                     </h1>
-                    <div className="contenedor006">
-                        <h2 className="texto-002">
-                            Encuentra todo lo que necesitas cerca de ti, en un solo lugar. El aplicativo perfecto encontrar los mejores productos. ¡Comprar nunca ha sido tan fácil!                    </h2>
+                    <h2 className="texto-002-res">
+                        Encuentra todo lo que necesitas cerca de ti, en un solo lugar. El aplicativo perfecto encontrar los mejores productos. ¡Comprar nunca ha sido tan fácil!                    </h2>
+                    <div></div>
+                    <div>
+                        <img className="img3" src={Img3} alt="Img3" />
                     </div>
-                    <div className="contenedor007">
-                        <button className="boton-01">Registrarme</button>
+                    <div className="contenedor-002-res">
+                        <button className="boton-1">Registrarme</button>
                         <img className="google" src={Google} alt="Google" />
+                    </div>
+
+
+                    <div>
+                        <h1 className="texto-001">
+                            <span>Kampanee</span> te acerca a tus proveedores favoritos
+                        </h1>
+                        <div className="contenedor006">
+                            <h2 className="texto-002">
+                                Encuentra todo lo que necesitas cerca de ti, en un solo lugar. El aplicativo perfecto encontrar los mejores productos. ¡Comprar nunca ha sido tan fácil!                    </h2>
+                        </div>
+                        <div className="contenedor007">
+                            <button className="boton-01">Registrarme</button>
+                            <img className="google" src={Google} alt="Google" />
+                        </div>
                     </div>
                 </div>
             </div>
+
+
             <div className="contenedorSub01">
                 <h1 className="texto-0001">
                     ¿Necesitas <span>ayuda?</span>
@@ -89,20 +111,21 @@ function Usuario() {
                     ¡Ponte en <span> contacto con nosotros! </span>
                 </h1>
             </div>
-            <div className="container">
-                <div className="cont-02">
-                    <img className="imgcon02" src={Img4} alt="img4" />
-                </div>
-                <div className="cont-03">
-                    <h1 className="text01">Soporte de Ayuda</h1>
-                    <h2 className="text02">¿Tienes problemas para usar nuestra app?</h2>
-                    <h3 className="text03">Te ayudaremos a resolver todas las dudas con nuestras soluciones, para que puedas sacar el máximo provecho a Kampanee.</h3>
-                    <img className="img-04" src={sub04} alt="sub04" />
-
+            <div>
+                <div  className="container">
+                    <div className="cont-02">
+                        <img className="imgcon02" src={Img4} alt="img4" />
+                    </div>
+                    <div className="cont-03">
+                        <h1 className="text01">Soporte de Ayuda</h1>
+                        <h2 className="text02">¿Tienes problemas para usar nuestra app?</h2>
+                        <h3 className="text03">Te ayudaremos a resolver todas las dudas con nuestras soluciones, para que puedas sacar el máximo provecho a Kampanee.</h3>
+                        <img className="img-04" src={sub04} alt="sub04" />
+                    </div>
                 </div>
             </div>
             <div>
-                <Footer/>
+                <Footer />
             </div>
 
         </div>
