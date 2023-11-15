19f9855b6c90b10cf922a81d34ebbeaacd8356ed
JoseCurySanchez5 <qryjeimpleis@gmail.com>
2023-11-15 14:30:34 -0500

    avanze negocios-responsive

--- src/componentes/Negocio.jsx
+++ src/componentes/Negocio.jsx
@@ -1,95 +1,291 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import first from "../img/first.png"
+import avion from "../img/avion.png"
 import '../Style/Negocio.css'
-import Logo from '../img/Logo.svg';
-import GooglePlay from '../img/Google-Play.svg'
-import HeadKampanee from '../img/Head-Kampanee.png';
-import Belleza from '../img/Belleza.png';
-import Bodegas from '../img/Bodegas.png';
-import Comidas from '../img/Comidas.png'
-import Farmacia from '../img/Farmacia.png';
-import Ferreteria from '../img/Ferreteria.png';
-import Importacion from '../img/Importacion.png';
-import Libreria from '../img/Libreria.png';
-import Otros from '../img/Otros.png';
-import Ropa from '../img/Ropa.png';
-import Tecnologia from '../img/Tecnologia.png';
-import KampaneeNegocios from '../img/logo-kampanee-negocios.png';
-import KampaneNegociosRight from '../img/Kampanee-negocios-right.png'
+import celular from "../img/mockup flyer 1.png"
+import phoneproductos from "../img/phoneproductos.png"
+import Footer from "./Footer";
+import tecnologia from '../img/Tecnologia.png'
+import ropa from '../img/Ropa.png'
+import bellesa from '../img/bellesa.png'
+import farmacia from '../img/Farmacia.png'
+import bodegas from '../img/Bodegas.png'
+import libreria from '../img/Libreria.png'
+import ferreteria from'../img/Ferreteria.png'
+import otros from '../img/Otros.png'
+import comida from '../img/Comidas.png'
+import audifonosazules from '../img/audifonosazules.png'
+import taladro from '../img/taladro.png'
+import hamburgesa from '../img/hamburgesa.png'
+import camisa from '../img/camisa.png'
+import nube from '../img/nube.png'
+import cohete from '../img/cohete.png'
+import telefonofunciona from '../img/telefonosfunciona.png'
+import React, { useState } from 'react';
+import Google from '../img/Google-Play.svg'
+import Logo from '../img/Logo.svg'
+import linea from '../img/linea.svg'
+import kampaneeicon from '../img/kampanee_icon.svg'
+import ventasicon from '../img/ventas_icon.svg'
+import inventarioicon from '../img/inventario_icon.svg'
+import negocioicon from '../img/negocio_icon.svg'
 
 
 function Negocio() {
+  const [mostrarContenido, setMostrarContenido] = useState(false);
+
+  const toggleContenido = () => {
+    setMostrarContenido(!mostrarContenido);
+  };
+  
   return (
-    <div className="secc-negocio">
-      {/*Head Negocio*/}
-      <div className="head-negocio m-5">
-        <img src={Logo} className='logo-kampanee' />
-        <div className="row">
-          <div className="col-md-6 head-left">
-            <h1>Descubre <span className='negrita'>Kampanee:</span>  Tu Conexión con los <span className='negrita'>Negocios Locales</span></h1>
-            <p>Kampanee es la plataforma que une a negocios locales con clientes. Descubre ofertas exclusivas, gestiona tus productos o encuentra tu próximo lugar favorito y todo en un solo lugar!</p>
-            <div className="buttons d-flex justify-content-start">
-              <button className='btn-explorar'>Explorar ahora</button>
-              <img className='ms-4 ' src={GooglePlay} />
-            </div>
-          </div>
-          <div className="col-md-6 head-right d-flex justify-content-center">
-            <img src={HeadKampanee} className='head-kampanee' alt="" />
-          </div>
-        </div>
-      </div>
-      {/* Fin de head Negocio*/}
-      {/* Seccion explora y descubre*/}
-      <div className="explorar">
-        <div className="texto d-flex justify-content-center text-center flex-column">
-          <h1>Explora y Descubre <span className='ex-negrita'>Nuevas Experiencias</span></h1>
-          <p>Encuentra tu próximo lugar favorito en Kampanee, donde podrás explorar una amplia variedad de negocios locales y descubrir nuevas experiencias.</p>
-        </div>
-        <div className="row cards" >
-          <div className="col-md-6">
-            <div className="row">
-              <div className="col d-flex justify-content-center flex-column"><img src={Importacion} className='img-cards' />Importación</div>
-              <div className="col d-flex justify-content-center flex-column"><img src={Tecnologia} className='img-cards' />Tecnología</div>
-              <div className="col d-flex justify-content-center flex-column"><img src={Ropa} className='img-cards' />Ropa y calzado</div>
-              <div className="col d-flex justify-content-center flex-column"><img src={Belleza} className='img-cards' />Belleza y cosméticos</div>
-              <div className="col d-flex justify-content-center flex-column"><img src={Farmacia} className='img-cards' />Farmacia</div>
-            </div>
-          </div>
-          <div className="col-md-6">
-            <div className="row">
-              <div className="col d-flex justify-content-center flex-column"><img src={Comidas} className='img-cards' />Comidas y bebidas</div>
-              <div className="col d-flex justify-content-center flex-column"><img src={Bodegas} className='img-cards' />Bodegas</div>
-              <div className="col d-flex justify-content-center flex-column"><img src={Libreria} className='img-cards' />Librería</div>
-              <div className="col d-flex justify-content-center flex-column"><img src={Ferreteria} className='img-cards' />Ferreterías</div>
-              <div className="col d-flex justify-content-center flex-column"><img src={Otros} className='img-cards' />Otros</div>
-            </div>
-          </div>
-        </div>
-        {/* Fin Seccion explora y descubre*/}
-        {/* Eres dueño de un negocio*/}
-        <div className="secc-d-negocio">
-          <div className="row d-flex">
-            <div className="col-md-6 kampanee-negocios-left">
-              <div className="left">
-                <img src={KampaneeNegocios} alt="" />
-                <h1>¿Eres dueño <span>de un negocio?</span></h1>
-                <p>¡Descubre cómo Kampanee Negocios puede ayudarte a impulsar tu éxito!</p>
-                <div className="buttons d-flex justify-content-start">
-                  <button className='btn-explorar'>Explorar ahora</button>
-                  <img className='ms-4 ' src={GooglePlay} />
+    <>
+                <div id="first" style={{
+                display: "flex",
+                flexDirection: "column",
+
+                
+            }}>
+              <div className="logo">
+                  <img  src={Logo}  />
+              </div>
+                <div className="kampaneeheader" style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    alignContent: "center",
+                    alignItems: "center",
+                    gap: "123px",
+                }}>
+                    <div id="header" style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        gap: "68px"
+                    }}>
+                        <h2 className="title" style={{
+                        }}>Descubre <span className="kampanee">Kampanee:</span> Tu Conexión con los <span className="kampanee" style={{ fontWeight: "700" }}> Negocios Locales</span></h2>
+                        <p className="paragraph" style={{
+                           color:"#565656", 
+                        }}>Kampanee es la plataforma que une a negocios locales con clientes. Descubre ofertas exclusivas, gestiona tus productos o encuentra tu próximo lugar favorito y todo en un solo lugar!</p>
+                        <div className="btnes1" style={{
+                            display: "flex",
+                            gap: "28px"
+                        }}>
+                            <div className="btnes1">
+                                <button id="btuno"className="kampanee-btn">Explorar ahora</button>
+                              <div className="btgoogle2">
+                                  <img  src={Google} />
+                              </div>
+                          </div>
+
+                      </div>
+                  </div>
+                  <img
+                      className="imagenheader"
+                      src={first}
+                      alt=""
+                      style={{
+                          width: "872px",
+                          height: "786px",
+                      }}
+                  />
+
                 </div>
-              </div>
             </div>
-            <div className="col-md-6 d-flex justify-content-end">
-              <div className="img-right">
-                <img className="img-right-negocios" src={KampaneNegociosRight} alt="" />
+            <div id="second" style={{
+                display: "flex",
+                flexDirection: "column",
+                marginTop: "99px",
+                marginLeft: "139px",
+                gap: "51px",
+                justifyContent: "center",
+
+                textAlign: "center",
+            }}>
+                <h2 className="title2" style={{
+                    width: "785px",
+                    display: "flex",
+                    flexDirection: "column",
+                    alignSelf: "center"
+                }}>
+                    <span className="exploraydescubre" style={{ fontWeight: "700" }}>Explora y Descubre</span>
+                    <span style={{ fontWeight: "500" }}>Nuevas Experiencias</span>
+                </h2>
+                <p className="paragraph2" >Encuentra tu próximo lugar favorito en Kampanee, donde podrás explorar una amplia variedad de negocios locales y descubrir nuevas experiencias.</p>
+                
+                <div className="cards">
+                    <div id="cardavion" className="card">
+                        <img src={avion} alt="" />
+                        <h4 className="cardname">Importacion</h4>
+                        
+                    </div>
+                    <div id="cardtecnologia" className="card">
+                        <img src={tecnologia} alt="" />
+                        <h4 className="cardname">Tecnologia</h4>
+
+                    </div>
+                    <div  id="cardRopa" className="card">
+                        <img src={ropa} alt="" />
+                        <h4 className="cardname">Ropa y calzado</h4>
+                    </div>
+                    <div id="cardbellesa" className="card">
+                        <img src={bellesa} alt="" />
+                        <h4 className="cardname">Belleza y<br>
+                        </br>cosméticos</h4>
+                    </div>
+                    <div id="cardfarmacia" className="card">
+                        <img src={farmacia} alt="" />
+                        <h4 className="cardname">Farmacia</h4>
+                    </div>
+                    <div id="cardComidas" className="card">
+                        <img src={comida} alt="" />
+                        <h4 className="cardname">Comidas<br></br> y bebidas</h4>
+                    </div>
+                    <div id="cardbodegas" className="card">
+                        <img src={bodegas} alt="" />
+                        <h4 className="cardname">Bodegas</h4>
+                    
+                    </div>
+                    <div id="cardlibreria" className="card">
+                        <img src={libreria} alt="" />
+                        <h4 className="cardname">Librería</h4>
+                    </div>
+                    <div id="cardferreterias" className="card">
+                        <img src={ferreteria} alt="" />
+                        <h4 className="cardname">Ferreterías</h4>
+                    </div>
+                    <div id="cardotros" className="card">
+                        <img src={otros} alt="" />
+                        <h4 className="cardname">Otros</h4>
+                    </div>
+                </div>
+            </div>
+
+            <div className="kampanee-negocios">
+                <div className="kampaneelogodueño">
+                    <h4 className="subtittlelogo">Negocios</h4>
+                    <img id="logonegocios" src={Logo}  />
+                </div>
+                <h1 className="dueñotitle">
+                    <span style={{ color: '#FF8000', fontWeight: 900 }}>¿Eres dueño</span> <span>de un negocio?</span>
+                </h1>
+                <p className="dueñodescripcion">¡Descubre cómo Kampanee Negocios Negocios puede ayudarte
+                  a impulsar tu exito!</p>
+              <div className="dueñobotones">
+                  <button id="btdos" className="kampanee-btn">Explorar ahora</button>
+                  <div className="googlebt">
+                      <img src={Google} />
+                  </div>
               </div>
+                <div className="celular1-container">
+                    <img className="celular1" id="audifonosazules" src={audifonosazules} alt="" />
+                    <img className="celular1" id="taladro" src={taladro} alt="" />
+                    <img className="celular1" id="hamburgesa" src={hamburgesa} alt="" />
+                    <img className="celular1" id="camisa" src={camisa} alt="" />
+                    <img className="celular1" id="nubenegocios1" src={nube} alt="" />
+                    <img className="celular1" id="nubenegocios2" src={nube} alt="" />
+                    <img className="dueñolinea"src={linea}></img>
+
+                    <img className="celular1" id="celular" src={celular} alt="" />
+                </div>
+            </div>
+
+            
+            <div className="descubre-kampanee">
+                <div className="kampaneeicon">
+                    <img src={kampaneeicon}></img>
+                </div>
+                <h1 className="descubretitle">
+                    Descubre todo lo que <span style={{ color: '#FF8000', fontWeight: 900 }}> Kampanee</span> tiene para ti
+                </h1>
+
+                <div className="iconytext">
+                    <div className="gestionventas">
+                      <div className="iconventas">
+                          <img src={ventasicon}></img>
+                      </div>
+                        <h4>Gestiona tus ventas</h4>
+                        <p>Brindale una mejor <br></br> experiencia de compra a tus <br></br>clientes.</p>
+                    </div>
+
+                    <div className="inventarioadmin">
+                      <div className="iconinventario">
+                          <img src={inventarioicon}></img>
+
+                      </div>
+                        <h4>Administra tu inventario</h4>
+                        <p>Registra tus productos en tu<br></br> inventario y optimiza tus costos.</p>
+                    </div>
+                    <div className="controlnegocio">
+                        <div className="iconcontrol">
+                            <img src={negocioicon}></img>
+                        </div>
+                        <h4>Ten el control de tu negocio</h4>
+                        <p>Controla el estado de tus ventas,<br></br> gastos y del capital invertido en tu<br></br>negocio.</p>
+                    </div>
+                </div>
+            </div>
+
+        <div className="me">
+            <div className="funcionakampanee">
+                <h1 className="funcionatittle">
+                    ¿Cómo funciona <span style={{ color: '#FF8000', fontWeight: 900 }}>Kampanee?</span>
+                </h1>
+                <div className="cartillas">
+
+                    <div className="cartilla-uno">
+                        <h4 className={`administra ${mostrarContenido ? 'active' : ''}`} style={{ color: '#FF8000', cursor: 'pointer' }} onClick={toggleContenido}>
+                            1. Administra tu negocio
+                        </h4>
+                        <div className={`administraparrafo-container ${mostrarContenido ? 'active' : ''}`}>
+                              <p className="administraparrafo" style={{ color: '#565656' }}>
+                                  ¡Kampanee permite a los negocios digitalizar sus finanzas.
+                                  Administrar ventas, gastos, deudas, inventario, balance mucho
+                                  más!
+                              </p>
+                        </div>
+                    </div>
+                    <div className="cartilla-dos">
+                        <h4>2. Muéstrate a potenciales clientes</h4>
+                    </div>
+                    <div className="cartilla-tres">
+                        <h4>3. Toma mejores decisiones</h4>
+                    </div>
+                </div>
+                <div className="imgfuncionakampanee">
+                        <img className="telefonos" src={telefonofunciona} alt="" />
+                </div>
 
             </div>
-          </div>
+
+
+            <div className="inventario-kampanee">
+                <h1 className="inventario-tittle">
+                    Controla tu <span style={{ color: '#FF8000', fontWeight: 900 }}>inventario</span> y <br /> optimiza tus costos
+                </h1>
+                <p className="inventario-parrafo">Con Kampanee visualiza tu inventario y mantente informado si
+                    cuentas con stock o tienes productos a punto de vencer. Registra la
+                    cantidad, el valor y la descripción de cada artículo para monitorear y
+                    hacer crecer las ganancias de tu negocio.</p>
+                <button className="btexplora">Explorar ahora</button>
+
+                <div className="phonocohete">
+                    <img className="lineainventario"src={linea}></img>
+
+                    <img className="nubeinventario" src={nube}></img>
+
+                    <img className="celphoneproductosular" src={phoneproductos} alt="" />
+                    <img className="cohete" src={cohete}></img>
+                </div>
+            </div>
         </div>
 
-      </div>
-    </div>
+        <div id="ayudacoainter" className="usuario-container">
+
+                <Footer />
+        
+        
+        </div>
+    </>
+
   )
 }
 
